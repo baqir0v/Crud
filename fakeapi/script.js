7f68e3c62f65a6b76a1e094cc959e10acb897687
Ahmad Baghirov <tu5r38eh9@code.edu.az>
2023-11-03 01:37:34 +0400

    CRUDs added

--- fakeapi/script.js
+++ fakeapi/script.js
@@ -1,6 +1,7 @@
 let base_URL = "http://localhost:3000/posts";
 let table = document.getElementById("table");
-let data = []; 
+let data = [];
+let currentEditPostId = null;
 
 async function FetchData(params) {
     try {
@@ -20,10 +21,38 @@ const responseData = () => {
             <td>${element.id}</td>
             <td>${element.title}</td>
             <td>${element.body}</td>
+            <td><button class="editButton" data-id="${element.id}">Edit</button></td>
+            <td><button class="deleteButton" data-id="${element.id}">Delete</button></td>
         `;
         table.appendChild(newRow);
     });
+
+    const editButtons = document.querySelectorAll(".editButton");
+    const deleteButtons = document.querySelectorAll(".deleteButton");
+
+    editButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const postId = button.getAttribute("data-id");
+            EditPost(postId);
+        });
+    });
+
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const postId = button.getAttribute("data-id");
+            deletePost(postId);
+        });
+    });
+    
+    
 }
+const updateButton = document.getElementById("updateButton");
+updateButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    updatePost(); 
+    FetchData(); 
+});
+
 
 async function createButton() {
     let nameInput = document.getElementById("Name").value;
@@ -40,5 +69,45 @@ async function createButton() {
     console.log("salam");
 }
 
+async function EditPost(postId) {
+    currentEditPostId = postId;
+    try {
+        const response = await axios.get(`${base_URL}/${postId}`)
+        const post = response.data
+
+        document.getElementById("Name").value = post.title
+        document.getElementById("ID").value = post.body
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function updatePost() {
+    if (currentEditPostId !== null) {
+        let titleInput = document.getElementById("Name").value;
+        let bodyInput = document.getElementById("ID").value;
+
+        try {
+            await axios.put(`${base_URL}/${currentEditPostId}`, {
+                title: titleInput,
+                body: bodyInput
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
+        currentEditPostId = null; 
+    }
+}
+
+async function deletePost(postId) {
+    try {
+        await axios.delete(`${base_URL}/${postId}`);
+        FetchData();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 FetchData();
